a15f167c57276fa0bccc9cd54970212bf6462f79
Sai Prasad <saiprasadanandan@gmail.com>
2021-03-07 23:58:39 +0530

    tar archive for log file

--- automation.sh
+++ automation.sh
@@ -14,3 +14,9 @@ sudo systemctl status apache2
 #To check if apache2 is enabled and running
 systemctl --type=service --state=running
 
+
+# tar archive for logs
+myname=upgrad-saiprasad
+timestamp=$(date '+%d%m%Y-%H%M%S')
+tar -cvf ${myname}-httpd-logs-${timestamp}.tar  --absolute-names /var/log/apache2/access.log
+tar -cvf ${myname}-httpd-logs-${timestamp}.tar  --absolute-names /var/log/apache2/error.log

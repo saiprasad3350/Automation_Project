962956a184ed9062de6c64f15a0c4fcb50193d94
Sai Prasad <saiprasadanandan@gmail.com>
2021-03-14 16:17:32 +0530

    new changes for task2

--- automation.sh
+++ automation.sh
@@ -5,18 +5,26 @@ sudo apt update -y
 dpkg -l apache2
 
 #To check apache version and install apache2
-apache2 -v
 sudo apt install apache2
 
 #To check if apache2 server is running
-sudo systemctl status apache2                                        
+sudo systemctl status apache2                                       
+sudo systemctl start apache2
 
 #To check if apache2 is enabled and running
 systemctl --type=service --state=running
+sudo systemctl enable apache2
+sudo systemctl restart apache2
 
+#To install aws cli 
+sudo apt update
+sudo apt install awscli
 
-# tar archive for logs
+#To archive dynamically
+
 myname=upgrad-saiprasad
+s3_bucket=upgrad-saiprasad
 timestamp=$(date '+%d%m%Y-%H%M%S')
-tar -cvf ${myname}-httpd-logs-${timestamp}.tar  --absolute-names /var/log/apache2/access.log
-tar -cvf ${myname}-httpd-logs-${timestamp}.tar  --absolute-names /var/log/apache2/error.log
+tar cvf $myname-httpd-logs-$timestamp.tar --absolute-names  /var/log/apache2
+cp $myname-httpd-logs-$timestamp.tar /tmp/
+aws s3 cp /tmp/${myname}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${myname}-httpd-logs-${timestamp}.tar
